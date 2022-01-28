d8f5795f1d3d2bb5b432be5493d31b254eb2c8c0
William Koller <williamkoller404@gmail.com>
2022-01-28 13:53:46 -0300

    feat: add mysql and update docker-compose

--- modules/docker/node/index.js
+++ modules/docker/node/index.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const mysql = require('mysql');
+
 const app = express();
 const port = 3000;
+
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb'
+};
+
+const connection = mysql.createConnection(config);
+
+const sql = `INSERT INTO people(name) VALUES('William')`;
+connection.query(sql);
+connection.end();
 
 app.get('/', (req, resp) => {
   resp.send('<h1>Full Cycle</h1/');
